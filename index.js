100421ece79139fc5fec607f2eac27dcf8a0e97f
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-01-21 22:31:47 +0530

    Complete index.js work

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "root",
+  port: 5432,
+});
+
 
 const app = express();
 const port = 3000;
 
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM flag", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  }
+  else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
 let totalCorrect = 0;
+
+
+
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
